612501e148f5e9f1977c7399f944d7b293b4735f
agenticuantico <agenticuantico@gmail.com>
2026-09-25 16:45:22 -0300

    fix: corregir sintaxis de paginas de categoria

--- app/tecnologia/page.tsx
+++ app/tecnologia/page.tsx
@@ -1,2 +1,2 @@
 import CategoryPage from '../../components/CategoryPage'
-export default function Page(){return <CategoryPage title='Tecnología' category='Tecnología' description='Inteligencia artificial, software, ciencia digital, ciberseguridad y productos que están cambiando la tecnología.'/>
+export default function Page(){return <CategoryPage title='Tecnología' category='Tecnología' description='Inteligencia artificial, software, ciencia digital, ciberseguridad y productos que están cambiando la tecnología.'/>}
